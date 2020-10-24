fc8917b3c224928ebeff9f183b16569177b43c27
debrawu <dwu1124@gmail.com>
2020-10-23 19:58:08 -0500

    adding scrape

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,16 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+def init_browser():
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+def scrape():
+    browser = init_browser()
+    
+
+    url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    browswer.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
